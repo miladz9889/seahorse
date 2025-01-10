5f2443ebf32b94df253be00b4ab32942394d7d3c
milad <milad@Hey.com>
2025-01-10 06:34:57 -0800

    adding event for ga button click

--- public/analytics.js
+++ public/analytics.js
@@ -4,3 +4,22 @@
   gtag('js', new Date());
 
   gtag('config', 'G-0QD8YWEY4V');
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const buyButton = document.getElementById("buy-seahorse");
+  
+    if (buyButton) {
+      buyButton.addEventListener("click", () => {
+        // Track the click event in Google Analytics
+        gtag('event', 'click', {
+          event_category: 'Button',
+          event_label: 'Buy $Seahorse',
+          value: 1
+        });
+  
+        //opening Jupiter Terminal
+        console.log('Buy $Seahorse button clicked');
+      });
+    }
+  });
